fcba4f38cc0cb7de0ea28c572a6b40a4ce604df5
Nuno Maduro <enunomaduro@gmail.com>
2017-12-19 00:33:58 +0100

    Adds unit tests

---
+++ tests/HandlerTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+use NunoMaduro\Collision\Writer;
+use NunoMaduro\Collision\Handler;
+use Symfony\Component\Console\Output\ConsoleOutput;
+use NunoMaduro\Collision\Contracts\Writer as WriterContract;
+use NunoMaduro\Collision\Contracts\Handler as HandlerContract;
+
+class HandlerTest extends TestCase
+{
+    /** @test */
+    public function it_respects_is_contract(): void
+    {
+        $this->assertInstanceOf(HandlerContract::class, new Handler());
+    }
+
+    /** @test */
+    public function it_handles_an_exception(): void
+    {
+        $writerMock = $this->createMock(WriterContract::class);
+        $inspectorMock = $this->createMock(\Whoops\Exception\Inspector::class);
+
+        $writerMock->expects($this->once())
+            ->method('write')
+            ->with($inspectorMock);
+
+        $handler = new Handler($writerMock);
+
+        $handler->setInspector($inspectorMock);
+
+        $this->assertEquals($handler->handle(), Handler::QUIT);
+    }
+
+    /** @test */
+    public function it_sets_the_output(): void
+    {
+        $writerMock = $this->createMock(WriterContract::class);
+        $output = new ConsoleOutput();
+
+        $writerMock->expects($this->once())
+            ->method('setOutput')
+            ->with($output);
+
+        (new Handler($writerMock))->setOutput($output);
+    }
+
+    /** @test */
+    public function it_gets_the_writer(): void
+    {
+        $writer = new Writer();
+        $handler = new Handler($writer);
+
+        $this->assertEquals($handler->getWriter(), $writer);
+    }
+}
---
+++ tests/ProviderTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Tests;
+
+use Whoops\Run;
+use Whoops\RunInterface;
+use PHPUnit\Framework\TestCase;
+use NunoMaduro\Collision\Handler;
+use NunoMaduro\Collision\Provider;
+use NunoMaduro\Collision\Contracts\Provider as ProviderContract;
+
+class ProviderTest extends TestCase
+{
+    /** @test */
+    public function it_respects_is_contract(): void
+    {
+        $this->assertInstanceOf(ProviderContract::class, new Provider());
+    }
+
+    /** @test */
+    public function it_registers_the_error_handler(): void
+    {
+        $handler = new Handler();
+
+        $runMock = $this->createMock(RunInterface::class);
+
+        $runMock->expects($this->once())
+            ->method('pushHandler')
+            ->with($handler)
+            ->willReturn($runMock);
+
+        $runMock->expects($this->once())
+            ->method('register');
+
+        (new Provider($runMock, $handler))->register();
+    }
+
+    /** @test */
+    public function it_gets_the_handler(): void
+    {
+        $handler = new Handler();
+        $provider = new Provider(new Run(), $handler);
+
+        $this->assertEquals($provider->getHandler(), $handler);
+    }
+}
