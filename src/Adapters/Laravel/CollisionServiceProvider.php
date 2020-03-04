37bdcfbe2e69c119f293ab3b0542ab0787318f02
Mior Muhammad Zaki <crynobone@gmail.com>
2020-03-04 08:16:09 +0800

    Use DeferrableProvider
    `$defer` property no longer used for Laravel 5.8 and above.

--- src/Adapters/Laravel/CollisionServiceProvider.php
+++ src/Adapters/Laravel/CollisionServiceProvider.php
@@ -12,6 +12,7 @@
 namespace NunoMaduro\Collision\Adapters\Laravel;
 
 use Illuminate\Contracts\Debug\ExceptionHandler as ExceptionHandlerContract;
+use Illuminate\Contracts\Support\DeferrableProvider;
 use Illuminate\Support\ServiceProvider;
 use NunoMaduro\Collision\Adapters\Laravel\Commands\TestCommand;
 use NunoMaduro\Collision\Contracts\Provider as ProviderContract;
@@ -27,15 +28,8 @@ use NunoMaduro\Collision\Writer;
  *
  * @author Nuno Maduro <enunomaduro@gmail.com>
  */
-class CollisionServiceProvider extends ServiceProvider
+class CollisionServiceProvider extends ServiceProvider implements DeferrableProvider
 {
-    /**
-     * {@inheritdoc}
-     *
-     * @var bool
-     */
-    protected $defer = true;
-
     /**
      * Boots application services.
      *
