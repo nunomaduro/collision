350da054da4706aa2c84560bc2acc0df96b84e90
Luke Downing <lukeraymonddowning@gmail.com>
2021-11-19 14:08:08 +0000

    Adds support for showing additional details when presenting exceptions in the console.

--- tests/LaravelApp/routes/web.php
+++ tests/LaravelApp/routes/web.php
@@ -3,7 +3,6 @@
 declare(strict_types=1);
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Validator;
 
 /*
 |--------------------------------------------------------------------------
