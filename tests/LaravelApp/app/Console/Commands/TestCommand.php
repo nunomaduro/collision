2b83a4228d3c212ca72447e20bb2906f3b50908b
Nuno Maduro <enunomaduro@gmail.com>
2023-03-19 17:16:52 +0000

    tests: fixes missing option

--- tests/LaravelApp/app/Console/Commands/TestCommand.php
+++ tests/LaravelApp/app/Console/Commands/TestCommand.php
@@ -23,6 +23,7 @@ class TestCommand extends BaseTestCommand
         {--profile : Lists top 10 slowest tests}
         {--recreate-databases : Indicates if the test databases should be re-created}
         {--drop-databases : Indicates if the test databases should be dropped}
+        {--without-databases : Indicates if database configuration should be performed}
         {--c|custom-argument : Add custom env variables}
 ';
 
