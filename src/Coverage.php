a2810432ecf7b95624eefb1fb8a9d253d39f32a1
nuno maduro <enunomaduro@gmail.com>
2026-04-06 20:23:01 +0100

    fix: code coverage on phpunit 13.1.0

--- src/Coverage.php
+++ src/Coverage.php
@@ -7,6 +7,7 @@ namespace NunoMaduro\Collision;
 use SebastianBergmann\CodeCoverage\CodeCoverage;
 use SebastianBergmann\CodeCoverage\Node\Directory;
 use SebastianBergmann\CodeCoverage\Node\File;
+use SebastianBergmann\CodeCoverage\Report\Facade;
 use SebastianBergmann\Environment\Runtime;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -89,10 +90,18 @@ final class Coverage
         $codeCoverage = require $reportPath;
         unlink($reportPath);
 
-        $totalCoverage = $codeCoverage->getReport()->percentageOfExecutedLines();
+        if (is_array($codeCoverage)) {
+            /** @var Facade $test */
+            $facade = Facade::fromSerializedData($codeCoverage);
 
-        /** @var Directory<File|Directory> $report */
-        $report = $codeCoverage->getReport();
+            /** @var Directory<File|Directory> $report */
+            $report = (fn () => $this->report)->call($facade);
+        } else {
+            /** @var Directory<File|Directory> $report */
+            $report = $codeCoverage->getReport();
+        }
+
+        $totalCoverage = $report->percentageOfExecutedLines();
 
         foreach ($report->getIterator() as $file) {
             if (! $file instanceof File) {
