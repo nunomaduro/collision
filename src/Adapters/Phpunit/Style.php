930ac942d01fb66de4f0dac24dc3d4e62b9baba5
nuno maduro <enunomaduro@gmail.com>
2026-07-15 20:06:53 +0100

    chore: style changes

--- src/Adapters/Phpunit/Style.php
+++ src/Adapters/Phpunit/Style.php
@@ -361,6 +361,7 @@ final class Style
             '/vendor\/brianium\/paratest/',
             '/vendor\/pestphp\/pest/',
             '/vendor\/pestphp\/pest-plugin-arch/',
+            '/vendor\/pestphp\/pest-plugin-evals/',
             '/vendor\/pestphp\/pest-plugin-browser/',
             '/vendor\/phpspec\/prophecy-phpunit/',
             '/vendor\/phpspec\/prophecy/',
